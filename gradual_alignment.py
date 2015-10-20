295578a86d5f117d6a6c980d2b07672c743b97d4
julianzaugg <julianzaugg@gmail.com>
2015-10-20 17:08:02 +1000

    Can perform bulk multiple alignment using various alorgithms. Can provide tab delimited file to dictate order of alignment. Various bug fixes.

--- gradual_alignment.py
+++ gradual_alignment.py
@@ -1,67 +1,167 @@
 """
 Gradual alignment of sequences using alignment methods.
-Requires MAFFT, MUSCLE, T-COFFEE installed to path (comment out or change paths 
-as necessary)
+Currently assumes MAFFT, MUSCLE and T-COFFEE installed to path (comment out or change paths
+as necessary; T-Coffee typically comes with with these other alignment tools provided)
 
 Will take a fasta file and do a gradual alignment of the sequences, storing the 
-length for each alignment and the CLUSTAL formatted alignments.
+CLUSTAL formatted alignments at each step.
 
 """
 import argparse
 import subprocess
+import os
+import sys
 
 from sequence import *
+from annotation import *
 
-
+OUT_LOCATION = "./"
+ANNOTATIONS = None
+ANNOTATION_DICT = None
+ORDER_ANNOTATION_NAME = None
 
 # Path locations for alignment algorithms, and additional parameters.
 # Currently assume mafft, muscle and t_coffee are install to path, but
 # comment out or change as desired.
-ALN_ALG_PATH_ARGS = { 
-    "linsi" : [], #Mafft
-    "muscle" : ["-maxiters 50"],
-    "t_coffee" : [],
+# {PATH : PARAMETERS}
+ALN_ALG_PATH_ARGS = {
+    "linsi": ["linsi", "--quiet", "--clustalout"], #Mafft
+    "muscle": ["muscle", "-maxiters 50", "-clw", "-clwstrict", "-quiet"],
+    "t_coffee": ["t_coffee", "-quiet"],
 }
 
+
+
+def _isfloat(x):
+    try:
+        a = float(x)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def _isint(x):
+    try:
+        a = float(x)
+        b = int(a)
+    except ValueError:
+        return False
+    else:
+        return a == b
+
+
+def _make_dir(name):
+    global OUT_LOCATION
+    if not os.path.exists(OUT_LOCATION + name):
+        os.makedirs(OUT_LOCATION + name)
 
 def _dir_check(directory):
     if not os.path.exists(directory):
         raise StandardError("The directory %s does not exist or the location \
-        	was incorrectly specified" % directory)
+            was incorrectly specified" % directory)
 
 def _parse_arguments(my_parser, my_args):
+    global OUT_LOCATION, ANNOTATIONS, ANNOTATION_DICT, ORDER_ANNOTATION_NAME
+    OUT_LOCATION = my_args.output
+
     # Load sequences from fasta file
-	input_seqs = read_fasta_file(my_args.input, Protein_Alphabet)
+    input_seqs = read_fasta_file(my_args.input, Protein_Alphabet)
+    # If an order file was provided
+    if my_args.order_file:
+        ANNOTATIONS = Annotation(my_args.order_file[0])
+        ORDER_ANNOTATION_NAME = my_args.order_file[1]
+        _name_column = ANNOTATIONS.get_column("Name")
+        _annotation_column = ANNOTATIONS.get_column(ORDER_ANNOTATION_NAME)
+        if _isfloat(_annotation_column[0]):
+            _annotation_column = map(float, _annotation_column)
+        ANNOTATION_DICT = dict(zip(_name_column, _annotation_column))
+        input_seqs = sorted(input_seqs, key=lambda x: ANNOTATION_DICT[x.name])
 
+    cnt = my_args.seqnumber
+    if my_args.alignment_methods == "all":
+        map(_make_dir, ["linsi", "muscle", "t_coffee"])
+    else:
+        _make_dir(my_args.alignment_methods)
+    while cnt < len(input_seqs):
+        # ALIGN THE INPUT WITH METHOD
+        cur_seqs = input_seqs[:cnt]
+        # Write temporary fasta file for infile
+        temp_in_file = OUT_LOCATION + "temp_cur_seqs.txt"
+        write_fasta_file(temp_in_file, cur_seqs)
+        print "Aligning first %i sequences" % len(cur_seqs)
+        if my_args.alignment_methods == "linsi":
+            # Call aligner
+            print "linsi\t%i" % cnt
+            command_args = ALN_ALG_PATH_ARGS["linsi"] + ["%s" % temp_in_file, ">",
+                                                         "%slinsi_%i.txt" % (OUT_LOCATION, cnt)]
+            subprocess.call(" ".join(command_args),
+                            shell=True)
+        if my_args.alignment_methods == "muscle":
+            # Call aligner
+            print "muscle\t%i" % cnt
+            command_args = ALN_ALG_PATH_ARGS["muscle"] + ["-in", "%s" % temp_in_file, "-out",
+                                                          "%smuscle_%i.txt" % (OUT_LOCATION, cnt)]
+            subprocess.call(" ".join(command_args),
+                            shell=True)
+        if my_args.alignment_methods == "t_coffee":
+            print "t_coffee\t%i" % cnt
+            # Call aligner
+            command_args = ALN_ALG_PATH_ARGS["t_coffee"] + ["-infile=%s" % temp_in_file, "-output=aln",
+                                                            "-outfile=%st_coffee_%i.txt" % (OUT_LOCATION, cnt)]
+            subprocess.call(" ".join(command_args),
+                            shell=True)
+        if my_args.alignment_methods == "all":
+            # Call aligner
+            print "linsi\t%i" % cnt
+            command_args1 = ALN_ALG_PATH_ARGS["linsi"] + ["%s" % temp_in_file, ">", "%slinsi_%i.txt" % (OUT_LOCATION +
+                                                                                                        "linsi/", cnt)]
+            print "muscle\t%i" % cnt
+            command_args2 = ALN_ALG_PATH_ARGS["muscle"] + ["-in", "%s" % temp_in_file, "-out", "%smuscle_%i.txt" %
+                                                           (OUT_LOCATION + "muscle/", cnt)]
+            print "t_coffee\t%i" % cnt
+            command_args3 = ALN_ALG_PATH_ARGS["t_coffee"] + ["-infile=%s" % temp_in_file, "-output=aln",
+                                                             "-outfile=%st_coffee_%i.txt" % (OUT_LOCATION +
+                                                                                             "t_coffee/", cnt)]
+            subprocess.call(" ".join(command_args1),
+                            shell=True)
+            subprocess.call(" ".join(command_args2),
+                            shell=True)
+            subprocess.call(" ".join(command_args3),
+                            shell=True)
+        cnt += 1
+        os.remove(temp_in_file)
+    pass
 
-	for s in input_seqs:
-		print s.name, len(s)
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Removes gaps from a clustal \
-                                            formmated sequence alignment file')
-	parser.add_argument('-i', '--input', help='Input alignment file', 
-    	required=True)
-	parser.add_argument('-o', '--output', help='Output Location', 
-    	required=False, default="./")
+    parser = argparse.ArgumentParser(description='Removes gaps from a clustal formmated sequence alignment file')
+    parser.add_argument('-i', '--input', help='Input alignment file', required=True)
+    parser.add_argument('-o', '--output', help='Output Location', required=False, default="./")
 
-	parser.add_argument('-alnm', '--alignment_methods', help='Alignment \
-		algorithms to use', required=True, choices = ("linsi", "muscle", 
-													"t_coffee", "all"))
+    parser.add_argument('-alnm', '--alignment_methods', help='Alignment algorithms to use', required=True,
+                        choices=("linsi", "muscle", "t_coffee", "all"))
 
-	parser.add_argument('-r', '--reference', help='Reference sequence name, if \
-	 provided will remove gaps in respect to this reference', required=False)
+    parser.add_argument('-r', '--reference', help='Reference sequence name, if provided will remove gaps in respect to '
+                                                  'this reference', required=False)
 
-    parser.add_argument('-order_file', help='if an order file is provided, ', required=False)
+    parser.add_argument('--order_file', nargs=2, help='if an order file is provided, sequences will be ordered '
+                                                        'first; Format should be a tab delimited text file where at '
+                                                        'least one column must have a header of "Name" with content '
+                                                        'matching sequence name. The user must also nominate another '
+                                                        'column to order sequences by, e.g., '
+                                                      'header =  "Name   Length" '
+                                                      'args = --order_file order_file_name Length',
+                                                        required=False)
+    parser.add_argument('-sn', '--seqnumber', help='Number of sequences to start alignment from',type=int, default=2)
+    # args = parser.parse_args()
 
-	# args = parser.parse_args()
-	
-	input_file = "/Users/julianzaugg/Documents/University/Phd/Projects/" +\
-        "Evolutionary_Pathway/Data/ANEH/Sequences/ESTHER/epoxide_fasta.txt"
-	args = parser.parse_args(["-i" , input_file, "-alnm", "linsi"])
+    input_file = "/Users/julianzaugg/Documents/University/Phd/Projects/Evolutionary_Pathway/" \
+                 "Data/ANEH/Sequences/ESTHER/epoxide_fasta.txt"
+    annotation_data = "/Users/julianzaugg/Documents/University/Phd/Projects/Evolutionary_Pathway/Data/ANEH/Sequences/BLAST_results/epoxide_blast_out_simple.txt"
+    args = parser.parse_args(["-i", input_file, "-alnm", "all", "-o", "/Users/julianzaugg/Desktop/my_test/", "--order_file", annotation_data, "EValue", "--seqnumber", 51])
 
-	_parse_arguments(parser, args)
+    _parse_arguments(parser, args)
 
 
 
